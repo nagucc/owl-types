dba33a62e8c9f920f7070e0fe3133933cd15773f
na57 <na57@qq.com>
2024-05-22 13:55:58 +0800

    init

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+import { INotion, ITriple } from 'nagu-triples-types';
+
+export interface IAnnotations {
+  label: string|INotion<string>,
+  comment: string|INotion<string>,
+  seeAlso: string|INotion<string>,
+  getAnnotations(): Promise<IAnnotations>,
+  setAnnotations(annotations: {
+    label?: string|INotion<string>,
+    comment?: string|INotion<string>,
+    isDefinedBy?: string|INotion<string>,
+    seeAlso?: string|INotion<string>,
+  }): Promise<void>,
+}
+
+export interface IRdfsResource extends IAnnotations {
+  iri: string|INotion<string>,
+  getPropertyValues(property: IRdfProperty|string): Promise<Array<IRdfsResource>>,
+  setPropertyValue(property: IRdfProperty|string, value: string | INotion<any>): Promise<ITriple>,
+  removePropertyValue(property: string | IRdfProperty, value: string | INotion<any>): Promise<number>,
+  types(): Promise<Array<IRdfsResource>>,
+  addType (type: IRdfsResource|INotion<string>|string): Promise<ITriple>,
+  removeType (type: IRdfsResource|INotion<string>|string): Promise<number>,
+  init(): Promise<void>,
+  destroy(): Promise<void>,
+}
+
+export interface IRdfsClass {
+  instances(): Promise<Array<IRdfsResource>>;
+}
+
+export interface IRdfProperty {
+  domians(): Promise<Array<IRdfsResource>>,
+  addDomain(resource: IRdfsClass|INotion<string>|string): Promise<ITriple>,
+  removeDomain(resource: IRdfsClass|INotion<string>|string): Promise<number>,
+  ranges(): Promise<Array<IRdfsResource>>,
+  addRange(resource: IRdfsResource|INotion<string>|string): Promise<ITriple>,
+  removeRange(resource: IRdfsResource|INotion<string>|string): Promise<number>,
+  check(): Promise<boolean>,
+}
