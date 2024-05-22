2c108d18071b27e5677829deabab09b287629d6c
na57 <na57@qq.com>
2024-05-22 16:25:13 +0800

    更新结构

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,20 @@
 import { INotion, ITriple } from 'nagu-triples-types';
 
-export interface IAnnotations {
+export type AnnotationProps = {
   label?: string|INotion<string>,
   comment?: string|INotion<string>,
-  isDefinedBy?: string|INotion<string>|IRdfsResource,
+  isDefinedBy?: string|INotion<string>|RdfsResourceProps,
   seeAlso?: string|INotion<string>,
-  getAnnotations(): Promise<IAnnotations>,
-  setAnnotations(annotations: IAnnotations): Promise<void>,
+}
+export interface IAnnotations {
+  getAnnotations(): Promise<AnnotationProps>,
+  setAnnotations(annotations: AnnotationProps): Promise<void>,
 }
 
-export interface IRdfsResource extends IAnnotations {
+export type RdfsResourceProps = {
   iri: string|INotion<string>,
+}
+export interface IRdfsResource extends IAnnotations, AnnotationProps, RdfsResourceProps {
   getPropertyValues(property: IRdfProperty|string): Promise<Array<IRdfsResource>>,
   setPropertyValue(property: IRdfProperty|string, value: string | INotion<any>): Promise<ITriple>,
   removePropertyValue(property: string | IRdfProperty, value: string | INotion<any>): Promise<number>,
